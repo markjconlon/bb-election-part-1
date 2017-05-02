10e5b3dfa27ff2d814b53eb480ae8b1d9a47452e
Mark <markjconlon@gmail.com>
2017-05-02 14:34:48 -0400

    displayed each candidate in the list displaying votes for name

--- election.js
+++ election.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-  // Imagination!
-
+  $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      for (var i = 0; i < data.candidates.length; i++) {
+        candidate = data.candidates[i]
+        $('<li>').html(candidate.name + ' has ' + candidate.votes + ' votes.').appendTo('#everyCandidate');
+      }
+    });
 });
